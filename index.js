917123e1e9dc4622f5627856a042419fca435acc
dylanmatthews <djmatthews93@gmail.com>
2023-09-11 15:48:17 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+let currentUser = "Dylan";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+let upperCaseCurrentUser = currentUser.toUpperCase()
+
+const firstInitial = currentUser[0];
+const restOfName= currentUser.slice(1);
+let shortGreeting = `Welcome, ${firstInitial}!`
+
+
+
+
+
+
+
+
+
